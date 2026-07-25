6fa4a239afc805a89b47e50137cb58f0f85b5053
AlexisPPLIN <contact@alexispoupelin.com>
2026-07-25 11:28:17 +0000

    Tweaks

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,4 +1,8 @@
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
 export default function(eleventyConfig) {
+    eleventyConfig.addPlugin(eleventyImageTransformPlugin);
     eleventyConfig.addPassthroughCopy("bundle.css");
+    eleventyConfig.addPassthroughCopy("bundle.js");
     eleventyConfig.addPassthroughCopy({ "_includes/img/favicon": "/" });
 };
